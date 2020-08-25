5002eff83286ff4f5fbd25b500fa561c60f566c9
cmor <clauartur98@gmail.com>
2020-08-25 14:47:02 -0500

    feat: add lifecycle widget

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -8,22 +8,18 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   void getLocation() async{
     Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
