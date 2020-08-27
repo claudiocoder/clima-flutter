4d7c795ddb45ef55a0617eba692461abb928ffeb
cmor <clauartur98@gmail.com>
2020-08-27 15:05:00 -0500

    feat: add network helper

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
-
+import 'package:clima/services/weather.dart';
 class LocationScreen extends StatefulWidget {
+  LocationScreen({this.locationWeather});
+  final locationWeather;
+
   @override
   _LocationScreenState createState() => _LocationScreenState();
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+
+  WeatherModel weather = WeatherModel();
+
+  int temperature;
+  String weatherIcon;
+  String cityName;
+
+  @override
+  void initState() {
+    super.initState();
+    updateUI(widget.locationWeather);
+  }
+
+  void updateUI(dynamic weatherData){
+    double temp = weatherData['main']['temp'];
+    temperature = temp.toInt();
+    var condition = 
+    weather.getWeatherIcon(weatherData['weather'][0]['id']);
+    cityName = weatherData['name'];
+  } 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,7 +73,7 @@ class _LocationScreenState extends State<LocationScreen> {
                 child: Row(
                   children: <Widget>[
                     Text(
-                      '32°',
+                      '$temperature°',
                       style: kTempTextStyle,
                     ),
                     Text(
@@ -74,3 +98,4 @@ class _LocationScreenState extends State<LocationScreen> {
     );
   }
 }
+
